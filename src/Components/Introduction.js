bcc94945e29db0b4f5566c1676f2d3c8f4941329
duart23 <duarte.peixoto.jorge@gmail.com>
2024-11-05 14:32:02 +0100

    introduction

--- src/Components/Introduction.js
+++ src/Components/Introduction.js
@@ -4,16 +4,15 @@ import Animoji from '../img/Animoji.png';
 
 function Introduction() {
   return (
-    <div className="introduction-container secondary d-flex">
+    <div className="introduction-container text-center secondary d-flex">
       <div className="introduction-body">
         <h1>Duarte Jorge</h1>
+        <h3 className="mb-0">Hi! Welcome to my Portfolio</h3>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-          accumsan vitae diam in varius. Phasellus aliquam dolor sed leo
-          vestibulum sagittis at at felis. Donec a luctus eros. Pellentesque sed
-          risus et nisl cursus ultricies. Aenean malesuada quis mi quis
-          interdum. Etiam elementum id risus at efficitur. Ut condimentum at
-          lorem tincidunt varius. Etiam sed lobortis massa.
+        I'm a 20-year-old Software Engineering student.
+         In this portfolio, you’ll find my projects, skills, and the path I’m taking 
+         toward a career in tech. Thanks for stopping by!
+
         </p>
       </div>
       <div className="introduction-image-container">
